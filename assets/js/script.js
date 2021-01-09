5f445f58c3836a78f235c78a0c1346a4cbae619e
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-01-08 23:58:53 -0600

    built questionAnswer object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 var timerEl = document.querySelector(".time");
 var timeRemaining = 75;
 
-var questionsAnswers
+var questionsAnswers = [
+    {
+        question: "Which of the following is NOT a Javascript data-type?",
+        choices: ["string", "object", "number", "boolean"],
+        answer: "object",
+    },
+    {
+        question:"What is a function?",
+        choices: ["A method", "An object property", "A continuous loop", "A block of code that does something"],
+        answer: "A block of code that does something",
+    }, {
+        question:"The __ method is used to add an item to the end of an array: ",
+        choices: ["concat", "slice", "push", "splice"],
+        answer: "push",
+    }, {
+        question:"Objects in Javascript can be used to store: ",
+        choices: ["arrays", "numbers", "functions", "all of the above"],
+        answer: "all of the above",
+    },
+]
 
 
 
